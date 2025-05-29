b5dcc8e7b92dbce3b21d81981de3d10a70aa065c
Bakir Chaban <B.Chaban@campus.lmu.de>
2025-05-29 23:01:34 +0200

    update

--- src/baseNN.py
+++ src/baseNN.py
@@ -10,6 +10,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+
+from ucimlrepo import fetch_ucirepo ,list_available_datasets
+
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 #REGRESSION
 #Standard NN, no need for extra Class since its a standard NN
@@ -50,7 +53,6 @@ def PredNormal(model,x):
     return pred.squeeze(),np.zeros_like(pred.squeeze())
 
 #CLASSIFICATION
-
 def GetNeuralNetworkClassification(input_dim: int, num_classes: int, device="cpu"):
     return nn.Sequential(
         nn.Linear(input_dim, 32),
@@ -60,12 +62,42 @@ def GetNeuralNetworkClassification(input_dim: int, num_classes: int, device="cpu
         nn.Linear(32, 32),
         nn.ReLU(),
         nn.Linear(32, 16),
-        nn.ReLU,
+        nn.ReLU(),
         nn.Linear(32, num_classes),# Logits
         #keine softmax, weil crossentropyLoss softmax inkludiert
     ).to(device)
 
 def TrainNNClass(model,loader,epochs=1000):
-    None
+    optimizer = optim.Adam(model.parameters(), lr=5e-3)
+
+
 def PredictClass(model,x):
     None
+
+
+list_available_datasets()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
